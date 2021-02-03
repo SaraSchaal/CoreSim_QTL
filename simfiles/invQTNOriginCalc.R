2b744225a962527bf3db5ea3904fcab58e841fe6
Sara Schaal <schaal.s@husky.neu.edu>
2021-02-03 13:11:23 -0500

    Add example run for origin dynamics

--- simfiles/invQTNOriginCalc.R
+++ simfiles/invQTNOriginCalc.R
@@ -1,34 +1,70 @@
 ### Code for analyzing inversion characteristics ###
+# Settings
 options(scipen = 999)
-## Download Data
-folder <- "./results/Inversion/20201115_FullSet/"
-df.invTime <- read.table(paste(folder, "FullSet_invTime.txt", sep = ""), header = TRUE)
-df.invData <- read.table(paste(folder, "FullSet_invData.txt", sep = ""), header = TRUE)
-df.params <- read.table(paste(folder, "invSimParams.txt", sep = ""), header = TRUE)
-dim(df.params)
-head(df.invTime)
-head(df.invData)
+# Libraries
+library(dplyr)
+library(ggplot2)
 
-#final.gen.invs <- subset(df.invData, subset = sim_gen == 50000)
-final.gen.invsTime <- subset(df.invTime, subset = sim_gen == 50000)
-df.allInv <- merge(df.invData, final.gen.invsTime, all.y = TRUE, by = c("seed", "inv_id"))
-df.allInvData <- merge(df.allInvData, df.params, all.x = TRUE, by = "seed")
-df.allInvData <- df.allInvData[, -which(names(df.allInvData) %in% "sim_gen.x")]
+## Download Data
+folder <- "./results/Origin/"
+df.InvOrigin <- read.table(paste(folder, "3384725_outputOriginInvQtn.txt", sep = ""), header = TRUE)
+df.invData <- read.table(paste(folder, "3384725_outputInvSumInfo.txt", sep = ""), header = TRUE)
+df.invTime <- read.table(paste(folder, "3384725_outputInvTime.txt", sep = ""), header = TRUE)
+#df.params <- read.table(paste(folder, "invSimParams.txt", sep = ""), header = TRUE)
+#dim(df.params)
+
+# Subset for final generation
+df.invFinalGen <- subset(df.invTime, subset = sim_gen == 50000)
+
+# get sum of effect size on the phenotype
+df.InvQTNsum <- aggregate(qtnSelCoef~inv_id, FUN = sum, data = df.InvOrigin)
+df.InvQTNnum <- aggregate(qtn_id~inv_id, FUN = length, data = df.InvOrigin)
+df.InvOriginGen <- aggregate(sim_gen~inv_id, FUN = mean, data = df.InvOrigin)
+df.InvQTNsumNum <- left_join(df.InvQTNsum, df.InvQTNnum, by = "inv_id")
+colnames(df.InvQTNsumNum)[3] <- "origin_num_qtns"
+df.InvQTNsumNumGen <- left_join(df.InvQTNsumNum, df.InvOriginGen, by = "inv_id")
+colnames(df.InvQTNsumNumGen)[4] <- "origin_gen"
+
+# merge inversion data with origin dynamics
+df.InvDataOrigin <- left_join(df.invFinalGen, df.InvQTNsumNumGen, by = "inv_id")
+#check
+dim(df.InvOrigin[df.InvOrigin$inv_id == 40748419,])
+
+# subset for MAF > 0.01
+df.InvDataOriginMAF <- df.InvDataOrigin[df.InvDataOrigin$freq > 0.01,]
+for(i in 1:nrow(df.InvDataOriginMAF)){
+  if(df.InvDataOriginMAF$freq_p1[i] > df.InvDataOriginMAF$freq_p2[i]){
+    df.InvDataOriginMAF$pop[i] <- "pop1"
+  } else {
+    df.InvDataOriginMAF$pop[i] <- "pop2"
+    
+  }
+}
 
 ## Plotting
-ggplot()
+par(mfrow = c(1,2))
+plot(origin_num_qtns~origin_gen, data = df.InvDataOriginMAF, xlab = "Inversion Origin Generation",
+     ylab = "Origin number of QTNs in Inversion", col = "cornflowerblue", pch = 19, ylim = c(0, 400))
+plot(num_qtns~origin_gen, data = df.InvDataOriginMAF, xlab = "Inversion Origin Generation",
+     ylab = "Final number of QTNs in Inversion", col = "dodgerblue4", pch = 19, ylim = c(0, 400))
 
-# merge inversion through time data with parameters
-df.invTimeParam <- merge(df.invTime, df.params, all.x = TRUE, by = c("seed"))
+ggplot(df.InvDataOriginMAF, aes(x = origin_gen, y = qtnSelCoef, group = pop)) + 
+  geom_point(aes(color = pop, size = inv_FST), alpha = 0.8) + 
+  scale_color_manual(values=c("navy", "red")) + 
+  theme_classic() +
+  theme(panel.background = element_blank(), 
+        strip.background = element_rect(colour = "white", fill = "grey92"),
+        text = element_text(size = 15)) +
+  labs(title = "Total Effect of Inversion QTNs on Phenotype",
+       y = "sum of each Inversion QTNs effects on phenotype",
+       x = "Inversion Origin Generation") +
+  guides(color = guide_legend(title = "Pop with Highest\nFrequency of Inv")) +
+  guides(size = guide_legend(title = "Inversion FST")) 
 
-# subset inversion qtn file for just the seeds of interest
-df.invQTNTimeSub <- subset(df.invQTNTime, subset = seed == c(3383645, 3383657, 3383669, 3383681, 3383693))
-colnames(df.invQTNData)[3] <- "qtn_id"
 
-# merge inversion QTN data with parameters
-movie.InvQTNSub.Data <- merge(df.invQTNTimeSub, df.params, all.x = TRUE, by = c("seed"))
-head(movie.InvQTNSub.Data)
+plot(qtnSelCoef~origin_gen, data = df.InvDataOriginMAF, xlab = "Inversion Origin Generation",
+     ylab = "Sum of QTN effects on phenotype in Inversion", col = "cyan4", pch = 19)
 
-df.invQTNData[df.invQTNData$]
-movie.InvQTN.Data <- merge(df.invQTNData, movie.InvQTNSub.Data, all.y = TRUE, by = c("seed", "qtn_id"))
+
+
 
